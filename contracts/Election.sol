1cbae329463083fbf561e39c7fe98a51e171209f
Prakash <prkjha25@gmail.com>
2019-03-25 01:20:53 +0530

    cast votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,6 +7,8 @@ contract Election {
         string name;
         uint voteCount;
     }
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     //store candidate
     mapping(uint => Candidate) public candidates;
     // count candidates
@@ -18,5 +20,18 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name,0);
+    }
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 } 
